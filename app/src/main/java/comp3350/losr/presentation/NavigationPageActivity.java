14c787df1e720f1c97f5850114b0aa93af6ab61a
mjbathtub <mbathie1999@gmail.com>
2020-06-27 15:58:53 -0500

    Added transition from navigation page to profile

--- app/src/main/java/comp3350/losr/presentation/NavigationPageActivity.java
+++ app/src/main/java/comp3350/losr/presentation/NavigationPageActivity.java
@@ -2,7 +2,9 @@ package comp3350.losr.presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import comp3350.losr.R;
 
@@ -12,5 +14,11 @@ public class NavigationPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_page);
+    }
+
+    public void buttonProfileOnClick(View v)
+    {
+        Intent profileIntent = new Intent(getApplicationContext(), ProfileActivity.class);
+        startActivity(profileIntent);
     }
 }
