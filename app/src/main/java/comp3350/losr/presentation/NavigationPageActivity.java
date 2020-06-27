36b50441fb3685743e4ea41789da1e837cd3ab0c
mjbathtub <mbathie1999@gmail.com>
2020-06-27 15:40:20 -0500

    renamed main activity and changed file structure for tests

--- app/src/main/java/comp3350/losr/presentation/NavigationPageActivity.java
+++ app/src/main/java/comp3350/losr/presentation/NavigationPageActivity.java
@@ -1,4 +1,4 @@
-package comp3350.losr;
+package comp3350.losr.presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import comp3350.losr.R;
 
-public class MainActivity extends AppCompatActivity {
+public class NavigationPageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_navigation_page);
     }
 }
