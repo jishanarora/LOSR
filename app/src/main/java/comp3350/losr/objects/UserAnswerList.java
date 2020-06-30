ee10d52436461201c2d471f85975671e22dd49b7
Ronnie <ron99ds@gmail.com>
2020-06-30 14:28:44 -0500

    Create UserAnswerList.java
    This will be used to update users answers to questions

---
+++ app/src/main/java/comp3350/losr/objects/UserAnswerList.java
@@ -0,0 +1,16 @@
+package comp3350.losr.objects;
+
+import java.util.ArrayList;
+
+public class UserAnswerList {
+    private ArrayList answers;
+
+    public UserAnswerList(ArrayList answers) {
+        this.answers = answers;
+    }
+
+    public void updateUsersAnswer(object Answer){
+        
+    }
+
+}
