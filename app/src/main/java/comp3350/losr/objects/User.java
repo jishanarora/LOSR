96dd97d9c56a4e83617339aa3520f929cf755c7b
Ronnie <ron99ds@gmail.com>
2020-06-30 15:56:04 -0500

    User fields update
    added answer list and changed variable name case

--- app/src/main/java/comp3350/losr/objects/User.java
+++ app/src/main/java/comp3350/losr/objects/User.java
@@ -1,8 +1,25 @@
 package comp3350.losr.objects;
 
+import java.util.ArrayList;
+
 public class User
 {
-    private String UserFirstName;
-    private String UserLastName;
-    private String UserEmail;
+    private String userFirstName;
+    private String userLastName;
+    private String userEmail;
+    private ArrayList answers;
+
+
+    public User(String userFirstName, String userLastName, String userEmail, ArrayList answers) {
+        this.userFirstName = userFirstName;
+        this.userLastName = userLastName;
+        this.userEmail = userEmail;
+        this.answers = answers;
+    }
+
+    //update a specific answer
+    public void updateAnswerList(Boolean answer, int spot){
+        answers.add(spot, answer);
+    }
+
 }
