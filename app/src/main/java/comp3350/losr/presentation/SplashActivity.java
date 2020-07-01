f3b05024a552324ec4d62351d4a21cf8e5fdfb3c
Jishan Arora <jishan.sikasu@gmail.com>
2020-07-01 16:20:18 -0500

    Splash Logo

---
+++ app/src/main/java/comp3350/losr/presentation/SplashActivity.java
@@ -0,0 +1,25 @@
+package comp3350.losr.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+
+import comp3350.losr.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        SystemClock.sleep(3000);
+        Intent signInIntent= new Intent(SplashActivity.this,SignInActivity.class);
+        startActivity(signInIntent);
+        finish();
+    }
+
+}
