2f14ab85a2b32bb5bc6b28ebc6b47ab0c3de80c3
mjbathtub <mbathie1999@gmail.com>
2020-07-02 16:57:45 -0500

    Created database stub.

--- app/src/main/java/comp3350/losr/objects/User.java
+++ app/src/main/java/comp3350/losr/objects/User.java
@@ -1,8 +1,24 @@
 package comp3350.losr.objects;
 
+import java.util.ArrayList;
+
 public class User
 {
-    private String UserFirstName;
-    private String UserLastName;
-    private String UserEmail;
+    private String userFirstName;
+    private String userLastName;
+    private String userEmail;
+    private ArrayList<Boolean> answers;
+
+
+    public User(String userFirstName, String userLastName, String userEmail, ArrayList<Boolean> answers) {
+        this.userFirstName = userFirstName;
+        this.userLastName = userLastName;
+        this.userEmail = userEmail;
+        this.answers = answers;
+    }
+
+    //update a specific answer
+    public void updateAnswerList(Boolean answer, int spot){
+        answers.add(spot, answer);
+    }
 }
