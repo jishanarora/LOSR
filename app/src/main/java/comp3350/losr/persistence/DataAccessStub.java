655042b3354e637eb04319166aeb27e3ebf555dc
mjbathtub <mbathie1999@gmail.com>
2020-07-03 22:08:40 -0500

    Added functionality to add and delete users from the db stub

--- app/src/main/java/comp3350/losr/persistence/DataAccessStub.java
+++ app/src/main/java/comp3350/losr/persistence/DataAccessStub.java
@@ -9,6 +9,7 @@ public class DataAccessStub {
     private String dbName;
     private String dbType = "stub";
 
+    private ArrayList<User> users;
 
     public DataAccessStub(String name)
     {
@@ -18,20 +19,37 @@ public class DataAccessStub {
     public void openConnection()
     {
         User newUser;
-        ArrayList<User> stub = new ArrayList<User>();
+
+        users = new ArrayList<>();
 
         newUser = new User("John", "Doe", "johndoe@gmail.com", new ArrayList<Boolean>());
-        stub.add(newUser);
+        users.add(newUser);
         newUser = new User("Mary", "Poppins", "marypoppins@gmail.com", new ArrayList<Boolean>());
-        stub.add(newUser);
+        users.add(newUser);
         newUser = new User("Gary", "Chalmers", "garychalmers@gmail.com", new ArrayList<Boolean>());
-        stub.add(newUser);
+        users.add(newUser);
         newUser = new User("Sean", "Lett", "seanlett@gmail.com", new ArrayList<Boolean>());
-        stub.add(newUser);
+        users.add(newUser);
         newUser = new User("Laura", "Stubbs", "laurastubbs@gmail.com", new ArrayList<Boolean>());
-        stub.add(newUser);
+        users.add(newUser);
 
         System.out.println("Opened connection to "+dbType+" database "+dbName);
+    }
+
+    public void addUser(User newUser)
+    {
+        users.add(newUser);
+    }
+
+    public void deleteUser(User selectedUser)
+    {
+        int index;
+
+        index = users.indexOf(selectedUser);
+        if(index >= 0)
+        {
+            users.remove(index);
+        }
     }
 
     public void closeConnection()
