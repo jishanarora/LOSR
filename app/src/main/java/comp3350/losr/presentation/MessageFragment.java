f6ccd33308c2e769d7d0ced9bebe8fa41395df62
Ronnie <ron99ds@gmail.com>
2020-07-09 13:10:06 -0500

    Match displaying
    configured match layout in listview and array adapter template

--- app/src/main/java/comp3350/losr/presentation/MessageFragment.java
+++ app/src/main/java/comp3350/losr/presentation/MessageFragment.java
@@ -1,31 +1,20 @@
 package comp3350.losr.presentation;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
 
 import comp3350.losr.R;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MessageFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MessageFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public MessageFragment() {
         // Required empty public constructor
@@ -46,31 +35,54 @@ public class MessageFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment MessageFragment.
+     * @return A new instance of fragment ProfileFragment.
      */
     // TODO: Rename and change types and number of parameters
     public static MessageFragment newInstance(String param1, String param2) {
         MessageFragment fragment = new MessageFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private ListView matchesListView;
+//    private String[] matches;
+//    private View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_message, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_message, container,false);
+        ArrayList<String> matchItems = getMatchesList();
+        matchesListView = (ListView)rootView.findViewById(R.id.matchesListView);
+        matchesListView.setAdapter(new ArrayAdapter<String>(getActivity(), R.layout.matches_listview_detail, matchItems));
+
+
+        return rootView;
     }
+
+    private ArrayList<String> getMatchesList(){
+//        AccessMatches matchesAccess = new AccessMatches();
+
+//        ArrayList<Match> matchList = (ArrayList<Match>) matchesAccess.getMatches();
+//        ArrayList<ListviewContactItem> contactlist = new ArrayList<ListviewContactItem>();
+        ArrayList<String> matches = new ArrayList<String>();
+
+        String match = new String();
+
+        match = "Guy 1";
+        matches.add(match);
+
+        match = new String();
+        match = "Guy 2";
+        matches.add(match);
+
+        match = new String();
+        match = "Guy 3";
+        matches.add(match);
+
+        return matches;
+    }
+
 }
