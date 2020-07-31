a1054957122532baf78bbf44b32b76c22f38e2ba
Jishan Arora <jishan.sikasu@gmail.com>
2020-07-30 22:14:15 -0500

    Macthed profile

--- app/src/main/java/comp3350/losr/presentation/MessageFragment.java
+++ app/src/main/java/comp3350/losr/presentation/MessageFragment.java
@@ -1,15 +1,19 @@
 package comp3350.losr.presentation;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
@@ -21,10 +25,7 @@ import comp3350.losr.objects.Match;
 public class MessageFragment extends Fragment
 {
 
-    public MessageFragment()
-    {
-        // Required empty public constructor
-    }
+    public View.OnClickListener myClickListener;
 
     public static MessageFragment newInstance()
     {
@@ -54,6 +55,7 @@ public class MessageFragment extends Fragment
     {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_message, container,false);
+        View messageView = inflater.inflate(R.layout.matches_listview_detail, container,false);
 
         AccessMatches matchesAccess = new AccessMatches();
         ArrayList<Match> matchList = (ArrayList<Match>) matchesAccess.getMatches();
@@ -72,7 +74,7 @@ public class MessageFragment extends Fragment
             super(context, 0, matches);
         }
 
-        public View getView(int position, View convertView, ViewGroup parent)
+        public View getView(final int position, View convertView, ViewGroup parent)
         {
 
             Match currMatch = getItem(position);
@@ -80,10 +82,22 @@ public class MessageFragment extends Fragment
             if (convertView == null)
             {
                 convertView = LayoutInflater.from(getContext()).inflate(R.layout.matches_listview_detail, parent, false);
+                convertView.findViewById(R.id.profileButton).setOnClickListener(myClickListener);
+                convertView.setClickable(true);
             }
 
-            TextView matchName = (TextView) convertView.findViewById(R.id.matchName);
-            TextView matchPercent = (TextView) convertView.findViewById(R.id.matchPercent);
+            TextView matchName =convertView.findViewById(R.id.matchName);
+            TextView matchPercent=convertView.findViewById(R.id.matchPercent);
+            myClickListener = new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent= new Intent(getContext(),MatchedProfileActivity.class);
+                    intent.putExtra("value",position);
+                    startActivity(intent);
+                }
+            };
+
+
 
             matchName.setText(String.format("  %s %s", currMatch.getMatchedUser().getUserFirstName(), currMatch.getMatchedUser().getUserLastName()));
             matchPercent.setText(Integer.toString(currMatch.getMatchPercent()) + "% Match");
