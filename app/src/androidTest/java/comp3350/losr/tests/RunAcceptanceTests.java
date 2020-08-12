447c6629ab5e11bd81fae63ec60390cd5360f9c3
mjbathtub <mbathie1999@gmail.com>
2020-08-12 17:46:26 -0500

    blind mode tests

--- app/src/androidTest/java/comp3350/losr/tests/RunAcceptanceTests.java
+++ app/src/androidTest/java/comp3350/losr/tests/RunAcceptanceTests.java
@@ -4,11 +4,12 @@ import org.junit.runners.Suite;
 import org.junit.runner.RunWith;
 
 import comp3350.losr.tests.acceptance.AccountAcceptanceTests;
+import comp3350.losr.tests.acceptance.BlindModeAcceptanceTests;
 import comp3350.losr.tests.acceptance.MatchAcceptanceTests;
 import comp3350.losr.tests.acceptance.ProfileAcceptanceTests;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({AccountAcceptanceTests.class, ProfileAcceptanceTests.class, MatchAcceptanceTests.class})
+@Suite.SuiteClasses({AccountAcceptanceTests.class, ProfileAcceptanceTests.class, MatchAcceptanceTests.class, BlindModeAcceptanceTests.class})
 public class RunAcceptanceTests
 {
     public RunAcceptanceTests()
